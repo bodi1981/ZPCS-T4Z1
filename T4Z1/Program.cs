1d191715a3da884ae65e005de84f7d4fae9a667a
Wojtek <wojtek.bogdziun@gmail.com>
2022-02-01 19:01:08 +0100

    Add project files.

---
+++ T4Z1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace T4Z1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Podaj liczbę: ");
+                if (int.TryParse(Console.ReadLine(), out int number))
+                {
+                    if (number % 2 == 0)
+                        Console.WriteLine($"{number} jest liczbą parzystą");
+                    else
+                        Console.WriteLine($"{number} jest liczbą nieparzystą");
+                }
+                else
+                    Console.WriteLine($"Nie podano prawidłowej liczby");
+
+                Console.WriteLine($"Naciśnij dowolny klawisz aby kontunować, w aby wyjść");
+                if (Char.ToUpper(Console.ReadKey(true).KeyChar) == 'W')
+                    return;
+            }
+        }
+    }
+}
